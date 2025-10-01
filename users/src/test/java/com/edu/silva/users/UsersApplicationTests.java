30761c5ab36c4f14a4424a561591373fd1c714e1
EDUARDO\edusl <eduardo@joycar.com.br>
2025-10-01 17:29:17 -0300

    first commit

---
+++ users/src/test/java/com/edu/silva/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.silva.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
