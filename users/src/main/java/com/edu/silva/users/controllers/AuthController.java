8f7a57d37709038066ce3a2c65b5bca10da3f481
EDUARDO\edusl <eduardo@joycar.com.br>
2025-10-26 16:13:22 -0300

    # Implement the user service

--- users/src/main/java/com/edu/silva/users/controllers/AuthController.java
+++ users/src/main/java/com/edu/silva/users/controllers/AuthController.java
@@ -2,8 +2,6 @@ package com.edu.silva.users.controllers;
 
 import com.edu.silva.users.domain.AuthDTO;
 import com.edu.silva.users.domain.LoginResponseDTO;
-import com.edu.silva.users.domain.RegisterRequestDTO;
-import com.edu.silva.users.domain.UserRole;
 import com.edu.silva.users.domain.entities.User;
 import com.edu.silva.users.infra.security.TokenService;
 import com.edu.silva.users.repositories.UserRepository;
@@ -12,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +21,10 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
 
-    private final UserRepository userRepository;
-
     private final TokenService tokenService;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService) {
+    public AuthController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
         this.tokenService = tokenService;
     }
 
