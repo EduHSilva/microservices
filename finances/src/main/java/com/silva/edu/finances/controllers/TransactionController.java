0546034059252e90a7d210b9ff503ecf73cc0669
EDUARDO\edusl <eduardo@joycar.com.br>
2025-12-16 14:44:40 -0300

    # feature: init the finances microservice

---
+++ finances/src/main/java/com/silva/edu/finances/controllers/TransactionController.java
@@ -0,0 +1,59 @@
+package com.silva.edu.finances.controllers;
+
+import com.edu.silva.common.DefaultResponse;
+import com.silva.edu.finances.domain.dtos.request.AddTransactionDTO;
+import com.silva.edu.finances.domain.dtos.request.UpdateTransactionDTO;
+import com.silva.edu.finances.domain.dtos.response.TransactionResponseDTO;
+import com.silva.edu.finances.services.TransactionService;
+import jakarta.validation.Valid;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+public class TransactionController {
+    private final TransactionService service;
+
+    private final String BASE_URL = "/transactions";
+
+    TransactionController(TransactionService service) {
+        this.service = service;
+    }
+
+    @GetMapping(BASE_URL)
+    ResponseEntity<@NonNull DefaultResponse> all(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<@NonNull TransactionResponseDTO> t = service.findAll(page, size);
+        return ResponseEntity.ok(new DefaultResponse("Find all transactions successfully", t, BASE_URL));
+    }
+
+    @GetMapping(BASE_URL + "/{id}")
+    ResponseEntity<@NonNull DefaultResponse> one(@PathVariable UUID id) {
+        return ResponseEntity.ok(new DefaultResponse("Transaction find successfully", service.findById(id), BASE_URL, id));
+    }
+
+    @PostMapping(BASE_URL)
+    ResponseEntity<@NonNull DefaultResponse> save(@RequestBody @Valid AddTransactionDTO dto) {
+        TransactionResponseDTO t = service.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new DefaultResponse("Transaction successfully saved", t, BASE_URL, t.getId())
+        );
+    }
+
+    @PutMapping(BASE_URL + "/{id}")
+    ResponseEntity<@NonNull DefaultResponse> replace(@RequestBody @Valid UpdateTransactionDTO request, @PathVariable UUID id) {
+        return ResponseEntity.ok(new DefaultResponse("Transaction successfully updated", service.update(id, request), BASE_URL, id));
+    }
+
+    @DeleteMapping(BASE_URL + "/{id}")
+    ResponseEntity<@NonNull DefaultResponse> delete(@PathVariable UUID id) {
+        service.delete(id);
+        return ResponseEntity.ok(new DefaultResponse("Transaction successfully deleted"));
+    }
+}
